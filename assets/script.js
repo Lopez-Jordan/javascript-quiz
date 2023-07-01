0f3284a56a22ea7187c1d14167662566ee9dd7d7
Jordan Lopez <jordanlopezemail@gmail.com>
2023-06-30 17:49:39 -0700

    fixed problem with link of js file to html file

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,19 @@
 
 // Question 1
     // format question 1 css
+
+
+
+// GLOBAL VARS
+var timeLeft = 100;
+
+
+
+function questionOne (){
     
+
+}
+
+var start = document.getElementById("begin");
+start.addEventListener("click", questionOne);
+
