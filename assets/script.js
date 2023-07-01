c89f6fcca7e76a2935d05956958bdf175ecf3488
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-01 13:01:22 -0700

    set up questions, starting on completing question 1

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,19 @@
 
 // GLOBAL VARS
 var timeLeft = 100;
+var timerDivEl = document.getElementById("timer-div");
 var start = document.getElementById("begin");
 var homeScreen = document.getElementById("homescreen");
 var start = document.getElementById("begin");
+var correctEl = document.querySelector("correct1");
+var incorrectEl1 = document.querySelector("incorrect1");
+var incorrectEl2 = document.querySelector("incorrect2");;
+var incorrectEl3 = document.querySelector("incorrect3");;
 var firstEl = document.getElementById("first");
-var timerDivEl = document.getElementById("timer-div");
+var secondEl = document.getElementById("second");
+var thirdEl = document.getElementById("third");
+var fourthEl = document.getElementById("fourth");
+var fifthEl = document.getElementById("fifth");
 
 function incorrect(){
     timeLeft -= 10;
@@ -25,6 +33,7 @@ function correct(){
 function questionOne (){
     homeScreen.style.display = "none";
     firstEl.style.display = "block";
+    
 
 
 }
